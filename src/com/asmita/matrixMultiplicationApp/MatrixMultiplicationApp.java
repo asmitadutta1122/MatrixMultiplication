5d6bcbc8e3f122bf2c9fbe7439a437429307ac6d
Asmita Dutta <asmitad@moback.com>
2016-11-15 13:21:09 +0530

    Complete the file handling part

--- src/com/asmita/matrixMultiplicationApp/MatrixMultiplicationApp.java
+++ src/com/asmita/matrixMultiplicationApp/MatrixMultiplicationApp.java
@@ -1,41 +1,43 @@
 package com.asmita.matrixMultiplicationApp;
 
-import com.asmita.matrixMultiplication.MatrixMultiplicationFixedThreads;
-import com.asmita.matrixMultiplication.MatrixMultiplicationSingleThread;
 import com.asmita.matrixMultiplication.MatrixMultiplicationVariableThreads;
+import com.asmita.matrixMultiplication.fileHandling.FileHandler;
 
 public class MatrixMultiplicationApp {
 
 	public static void main(String[] args) {
-		int [][] mat1 = new int [3][4];
-		int [][] mat2 = new int [4][3];
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 4; j++) {
-				mat1[i][j] = 1000;
+		String input1 = null;
+		String input2 = null;
+		String output = null;
+		int numThreads = 0;
+		if (args.length != 8) {
+			throw new IllegalArgumentException("Insufficient options, please provide --input1 <file1>"
+					+ " --input2 <file2> --output <outfile> --threads <number of threads>");
+		}
+		for (int i = 0; i < args.length; i++) {
+			if(args[i].trim().equals("--input1")) {
+				input1 = args[++i];
+			} else if(args[i].trim().equals("--input2")) {
+				input2 = args[++i];
+			} else if(args[i].trim().equals("--output")) {
+				output = args[++i];
+			} else if(args[i].trim().equals("--threads")) {
+				numThreads = Integer.parseInt(args[++i]);
+			} else {
+				throw new IllegalArgumentException("Not a valid option: "+args[i] + " only --input1, --input2,"
+						+ " --threads and --output are allowed");
 			}
 		}
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 3; j++) {
-				mat2[i][j] = 1;
-			}
-		}
-		//MatrixMultiplicationFixedThreads mul = new MatrixMultiplicationFixedThreads(mat1, mat2);
-		MatrixMultiplicationVariableThreads mul = new MatrixMultiplicationVariableThreads(mat1, mat2, 2);
+	        	
+		int [][] mat1 = FileHandler.readInputMatrix(input1);;
+		int [][] mat2 = FileHandler.readInputMatrix(input2);;
+		
+		MatrixMultiplicationVariableThreads mul = new MatrixMultiplicationVariableThreads(mat1, mat2, numThreads);
 		int [][] result = mul.multiply();
-		System.out.println(toString(mat1));
-		System.out.println(toString(mat2));
-		System.out.println(toString(result));
+		FileHandler.writeOutputMatrix(result, output);
+		System.out.println("###");
+		System.out.println(FileHandler.toString(mat1));
+		System.out.println(FileHandler.toString(mat2));
+		System.out.println(FileHandler.toString(result));
 	}
-	
-	public static String toString(int [][] m) {
-        String result = "";
-        for(int i = 0; i < m.length; i++) {
-            for(int j = 0; j < m[i].length; j++) {
-                result += String.format("%10d", m[i][j]);
-            }
-            result += "\n";
-        }
-        return result;
-    }
-	
 }
